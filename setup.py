2a02631841d1cc11b307dd0014bcedd4c61bc873
Mario Finelli <mario@finel.li>
2016-03-27 13:28:18 -0400

    Rename to music_rename

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='music-rename',
+    name='music_rename',
     version='1.0.0',
     description='Rename music files after ripping with EAC.',
     url='https://github.com/mfinelli/music-rename',
     author='Mario Finelli',
     license='GPL',
     install_requires=[],
-    packages=['music-rename'],
+    packages=['music_rename'],
 )
