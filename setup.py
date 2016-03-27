c19afa2768ec54bb16d371bc17accf365b3e769a
Mario Finelli <mario@finel.li>
2016-03-27 12:59:49 -0400

    Add initial directory structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='music-rename',
+    version='1.0.0',
+    description='Rename music files after ripping with EAC.',
+    url='https://github.com/mfinelli/music-rename',
+    author='Mario Finelli',
+    license='GPL',
+    install_requires=[],
+    packages=['music-rename'],
+)
