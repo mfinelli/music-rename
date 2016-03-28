1b5cdd8803f4ba2258e9122c4d5fd4a6574e8fff
Mario Finelli <mario@finel.li>
2016-03-27 20:51:59 -0400

    Add argparsing skeleton

--- music_rename/console.py
+++ music_rename/console.py
@@ -1,2 +1,9 @@
+import os
+import argparse
+
 def main():
-    print("Hello")
+    parser = argparse.ArgumentParser(description='some help...',epilog="Distributed under the GPL")
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 2.0')
+    parser.add_argument('-d', '--directory', help='foo help')
+    args = parser.parse_args()
+    print("Current directory: " + os.getcwd())
