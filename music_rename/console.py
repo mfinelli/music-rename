f1db6d3992fb40048c853d4ab502f3e846f0717f
Mario Finelli <mario@finel.li>
2016-03-27 22:23:59 -0400

    Add GPL notices to files and help text

--- music_rename/console.py
+++ music_rename/console.py
@@ -1,9 +1,35 @@
+# music-rename: rename music files after ripping
+# Copyright (C) 2016 Mario Finelli
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import argparse
 import music_rename
 
+MAIN_DESCRIPTION = """Use music-rename to organize and rename music files
+    after ripping them."""
+
+EPILOG = """music-rename:  Copyright (C) 2016 Mario Finelli.
+    This program comes with ABSOLUTELY NO WARRANTY.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions. For details see the copy of the GNU
+    General Public License that you should have received along with
+    this program. (If not, see <http://www.gnu.org/licenses/>)"""
+
 def main():
-    parser = argparse.ArgumentParser(description='some help...',epilog="Distributed under the GPL")
+    parser = argparse.ArgumentParser(description=MAIN_DESCRIPTION,epilog=EPILOG)
     parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + music_rename.__version__)
     parser.add_argument('-d', '--directory', help='foo help')
     args = parser.parse_args()
