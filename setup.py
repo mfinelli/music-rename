41ff8f347cac22e53ebc496d4706f76ce0abb10a
Mario Finelli <mario@finel.li>
2016-03-29 23:25:51 -0400

    Cleanup code style for setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 import music_rename
 from setuptools import setup
 
-setup(
-    name='music_rename',
-    version=music_rename.__version__,
-    description='Rename music files after ripping with EAC.',
-    url='https://github.com/mfinelli/music-rename',
-    author='Mario Finelli',
-    license='GPL',
-    install_requires=[
-        'colorama',
-        'termcolor'
-    ],
-    packages=['music_rename'],
-    entry_points={
-        'console_scripts': [
-            'music-rename=music_rename.console:main'
-        ]
-    }
-)
+setup(name='music_rename',
+      version=music_rename.__version__,
+      description='Rename music files after ripping with EAC.',
+      url='https://github.com/mfinelli/music-rename',
+      author='Mario Finelli',
+      license='GPL',
+      install_requires=[
+          'colorama', 'termcolor'
+      ],
+      packages=['music_rename'],
+      entry_points={
+          'console_scripts': [
+              'music-rename=music_rename.console:main'
+          ]
+      })
