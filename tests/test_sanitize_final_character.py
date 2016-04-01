1ab1c55bfe9d608ec546c8bf80be6997a42a1597
Mario Finelli <mario@finel.li>
2016-03-31 20:31:58 -0400

    Add more tests for sanitize final character

--- tests/test_sanitize_final_character.py
+++ tests/test_sanitize_final_character.py
@@ -2,3 +2,21 @@ from music_rename import sanitize
 
 def test_good_string():
     assert sanitize.sanitize_final_character('abc') == 'abc'
+
+def test_remove_illegal_final_character():
+    assert sanitize.sanitize_final_character('abc abd(') == 'abc abd'
+
+def test_remove_trailing_whitespace():
+    assert sanitize.sanitize_final_character('abc   ') == 'abc'
+
+def test_remove_initial_whitespace():
+    assert sanitize.sanitize_final_character('   abc') == 'abc'
+
+def test_multiple_remove_rounds():
+    assert sanitize.sanitize_final_character('abc&&&&') == 'abc'
+
+def test_multipe_remove_whitespace_rounds():
+    assert sanitize.sanitize_final_character('abc [ [') == 'abc'
+
+def test_leave_middle_characters_alone():
+    assert sanitize.sanitize_final_character('abc \' abc') == 'abc \' abc'
