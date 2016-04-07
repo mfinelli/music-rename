18ee6b007acfdca729593162c6cb30fdcab18d42
Mario Finelli <mario@finel.li>
2016-04-07 08:44:14 -0400

    Add additional test for removing unrecognized

--- tests/test_remove_unrecognized_characters.py
+++ tests/test_remove_unrecognized_characters.py
@@ -5,5 +5,10 @@ def test_no_change():
     assert sanitize.remove_unrecognized_characters('abc') == 'abc'
 
 
+def test_keep_many():
+    assert sanitize.remove_unrecognized_characters(
+        '0abC,&.-()[]') == '0abC,&.-()[]'
+
+
 def test_remove_many():
     assert sanitize.remove_unrecognized_characters('ab{}*^$# !a') == 'ab a'
