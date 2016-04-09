19533318ab7e9744bf49daef11ef5398ae1a23b2
Mario Finelli <mario@finel.li>
2016-04-09 12:26:56 -0400

    Add additional tests for sanitize function

--- tests/test_sanitize.py
+++ tests/test_sanitize.py
@@ -8,3 +8,19 @@ def test_truncate():
 
 def test_no_trailing_dash():
     assert sanitize.sanitize('abc-', 10) == 'abc'
+
+
+def test_remove_illegal_characters():
+    assert sanitize.sanitize('ab * * cd', 5) == 'ab cd'
+
+
+def test_accented_characters():
+    assert sanitize.sanitize('àbcdë', 5) == 'abcde'
+
+
+def test_beginning_parenthesis():
+    assert sanitize.sanitize('abc()', 4) == 'abc'
+
+
+def test_ending_parenthesis():
+    assert sanitize.sanitize('abc()', 5) == 'abc()'
