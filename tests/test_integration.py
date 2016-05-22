87edc34db32118cafcd14ac621e1721ef512bed2
Mario Finelli <mario@finel.li>
2016-05-22 18:32:29 -0400

    Add traverse artist integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,15 +1,35 @@
 import pytest
 import tempfile
 import shutil
+import os
+import music_rename
+from music_rename import artists
+from music_rename import config
 
 @pytest.fixture(scope="module", autouse=True)
 def dir(request):
     dir = tempfile.mkdtemp()
+
+    os.mkdir(os.path.join(dir, 'Fine'))
+    os.mkdir(os.path.join(dir, 'With Space'))
+    os.mkdir(os.path.join(dir, 'With_Underscore'))
+    os.mkdir(os.path.join(dir, 'Sömé Àccents'))
+    os.mkdir(os.path.join(dir, 'Some directory that is way too many characters'))
+
+    prev = os.getcwd()
+    os.chdir(dir)
+    config = music_rename.config.get_populated_configuration()
+    music_rename.artists.get_artist_directories(config, True)
+
     def cleanup():
+        os.chdir(prev)
         shutil.rmtree(dir)
     request.addfinalizer(cleanup)
     return dir
 
-def test_one(dir):
-    assert True
-
+def test_artists(dir):
+    assert os.path.exists(os.path.join(dir, 'Fine'))
+    assert os.path.exists(os.path.join(dir, 'With Space'))
+    assert os.path.exists(os.path.join(dir, 'WithUnderscore'))
+    assert os.path.exists(os.path.join(dir, 'Some Accents'))
+    assert os.path.exists(os.path.join(dir, 'Some directory that is way too m'))
